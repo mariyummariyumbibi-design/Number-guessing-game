7c67135b42c76ace7c85943d486666b0fb8ed814
Mariyum Bibi <mariyummariyumbibi@gmail.com>
2026-02-13 20:48:58 +0500

    a code is written in main and it works

--- number_guesing.c
+++ number_guesing.c
@@ -2,3 +2,24 @@
 #include<stdlib.h>
 #include<time.h>
 
+int main(){
+    srand(time(0));
+    int number=rand()%100;
+    printf("guess a number between (1 to 99):\n");
+    int guess=-1;
+    int count=0;
+    while(number!=guess){
+        scanf("%d",&guess);
+        count++;
+        if(number>guess){
+            printf("guess the greater number:\n");
+        }else if(number<guess){
+            printf("guess the smaller number:\n");
+        }else{
+            printf("congratulations!,you guessed the number in %d attempts",count);
+        }
+
+    }
+    return 0;
+}
+
